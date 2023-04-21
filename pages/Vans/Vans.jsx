879bbd2dc956f53229e2651d14c0aa181e5acf7c
John U <arjorb@gmail.com>
2023-04-21 11:33:49 +0200

    change the link for the searchParams to be the button so that I can be able to use the setSearchParams

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -33,18 +33,18 @@ const Vans = () => {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <Link to="?type=simple" className="van-type simple">
+        <button onClick={() => setSearchParams({ type: "simple" })} className="van-type simple">
           Simple
-        </Link>
-        <Link to="?type=luxury" className="van-type luxury">
+        </button>
+        <button onClick={() => setSearchParams({ type: "luxury" })} className="van-type luxury">
           luxury
-        </Link>
-        <Link to="?type=rugged" className="van-type rugged">
+        </button>
+        <button onClick={() => setSearchParams({ type: "rugged" })} className="van-type rugged">
           rugged
-        </Link>
-        <Link to="?" className="van-type clear-filters">
+        </button>
+        <button onClick={() => setSearchParams({})} className="van-type clear-filters">
           Clear Filter
-        </Link>
+        </button>
       </div>
       <div className="van-list">{vanElements}</div>
     </div>

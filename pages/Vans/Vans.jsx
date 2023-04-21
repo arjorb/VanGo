0b9f181029843ff7d59441f69cc43690e402d64c
John U <arjorb@gmail.com>
2023-04-21 11:04:39 +0200

    done implementing the searchParams for type on the vans

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = React.useState([]);
+  const [searhParams, setSearchParams] = useSearchParams();
+  const typeFilter = searhParams.get("type");
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => (
+  const displayVan = typeFilter ? vans.filter((van) => van.type.toLowerCase() === typeFilter) : vans;
+
+  const vanElements = displayVan.map((van) => (
     <div key={van.id} className="van-tile">
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
@@ -28,6 +32,20 @@ const Vans = () => {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link to="?type=simple" className="van-type simple">
+          Simple
+        </Link>
+        <Link to="?type=luxury" className="van-type luxury">
+          luxury
+        </Link>
+        <Link to="?type=rugged" className="van-type rugged">
+          rugged
+        </Link>
+        <Link to="?" className="van-type clear-filters">
+          Clear Filter
+        </Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );

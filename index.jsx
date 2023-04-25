6d99bf13c03a6cc7aac27646e81313bb52c36158
John U <arjorb@gmail.com>
2023-04-25 11:45:21 +0200

    updating the dashboard, income and review component content

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans, { loader as vansLoader } from "./pages/Vans/Vans";
 import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail";
-import Dashboard from "./pages/Host/Dashboard";
+import Dashboard, { loader as dashboardLoader } from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import HostVans, { loader as hostVansLoader } from "./pages/Host/HostVans";
@@ -29,14 +29,14 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} loader={loginLoader} action={loginAction} />
       <Route path="vans" element={<Vans />} errorElement={<Error />} loader={vansLoader} />
-      <Route path="vans/:id" element={<VanDetail />} loader={vanDetailLoader} />
+      <Route path="vans/:id" element={<VanDetail />} errorElement={<Error />} loader={vanDetailLoader} />
 
       <Route path="host" element={<HostLayout />}>
-        <Route index element={<Dashboard />} loader={async ({ request }) => await requireAuth(request)} />
+        <Route index element={<Dashboard />} loader={dashboardLoader} />
         <Route path="income" element={<Income />} loader={async ({ request }) => await requireAuth(request)} />
         <Route path="reviews" element={<Reviews />} loader={async ({ request }) => await requireAuth(request)} />
-        <Route path="hostVans" element={<HostVans />} loader={hostVansLoader} />
-        <Route path="hostVans/:id" element={<HostVanDetail />} loader={hostVanDetailLoader}>
+        <Route path="hostVans" element={<HostVans />} errorElement={<Error />} loader={hostVansLoader} />
+        <Route path="hostVans/:id" element={<HostVanDetail />} errorElement={<Error />} loader={hostVanDetailLoader}>
           <Route index element={<HostVanInfo />} loader={async ({ request }) => await requireAuth(request)} />
           <Route path="pricing" element={<HostVanPricing />} loader={async ({ request }) => await requireAuth(request)} />
           <Route path="photos" element={<HostVanPhotos />} loader={async ({ request }) => await requireAuth(request)} />
--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,34 +1,41 @@
-import { useState, useEffect } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
 
 export const loader = async ({ request }) => {
   await requireAuth(request);
-  return getHostVans();
+  return defer({ vans: getHostVans() });
 };
 
 const HostVans = () => {
-  const vans = useLoaderData();
+  const dataPromise = useLoaderData();
 
-  const hostVansEls = vans.map((van) => (
-    <Link to={`${van.id}`} key={van.id} className="host-van-link-wrapper">
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
+  const renderVanElements = (vans) => {
+    const hostVansEls = vans.map((van) => (
+      <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
         </div>
+      </Link>
+    ));
+    return (
+      <div className="host-vans-list">
+        <section>{hostVansEls}</section>
       </div>
-    </Link>
-  ));
+    );
+  };
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
-      <div className="host-vans-list">
-        <section>{hostVansEls}</section>
-      </div>
+      <Suspense fallback={<h2>Loading vans...</h2>}>
+        <Await resolve={dataPromise.vans}>{renderVanElements}</Await>
+      </Suspense>
     </section>
   );
 };
